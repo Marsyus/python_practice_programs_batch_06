69e5474280d5b546570a17e62d8bb1ebeeb4b8de
Marsyus <miadolomboy0917@gmail.com>
2025-03-28 21:01:13 +0800

    Program Pseudocode

--- swapcase_alternative_02.py
+++ swapcase_alternative_02.py
@@ -1,1 +1,3 @@
 #Prog09. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
+#Create a function to replicate swapcase()
+#Utilize the created function
--- title_alternative.py
+++ title_alternative.py
@@ -1,1 +1,3 @@
 #Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
+#Create a function to replicate title()
+#Utilize the created function
