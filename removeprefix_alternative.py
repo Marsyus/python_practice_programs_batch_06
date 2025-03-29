f387a6761f25e9ac8c26f59469576d55f9f23641
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 11:25:27 +0800

    Created function removeprefix_replicate()

--- removeprefix_alternative.py
+++ removeprefix_alternative.py
@@ -1,3 +1,9 @@
 #Prog02. removeprefix() remove the characters at the beginning of the string that matches the function parameter. Create a program that do the same functionality without using removeprefix() function.
 #Create a function to replicate removeprefix()
+def removeprefix_replicate(prefix, remove):
+    if prefix.startswith(remove):
+        output = prefix.replace(remove, "")
+    else:
+        output = prefix
+    print(output)
 #Utilize the created function
