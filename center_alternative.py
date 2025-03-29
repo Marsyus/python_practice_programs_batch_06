036874b33609b4da184375cb5afeb03be47c5487
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 18:26:24 +0800

    Determine the count of character needed to complete the span to center

--- center_alternative.py
+++ center_alternative.py
@@ -4,4 +4,10 @@ def center_replicate(prefix, width, char):
     right = 0
     left = 0
     need = width - len(prefix)
+    if need % 2 == 0:
+        right = need // 2
+        left = right
+    else:
+        right = need // 2
+        left = right + 1
 #Utilize the created function
