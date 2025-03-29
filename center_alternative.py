5c0a72f6ab69b6f539fe1de23c13c16647cb7bfe
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 16:52:20 +0800

    Created function center_replicate()

--- center_alternative.py
+++ center_alternative.py
@@ -1,3 +1,5 @@
 #Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 #Create a function to replicate center()
+def center_replicate(prefix):
+    pass
 #Utilize the created function
