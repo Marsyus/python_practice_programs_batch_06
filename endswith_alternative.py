5c38b50c64c45208aa015fcf3d87a15af2b655b1
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 16:53:36 +0800

    Created function endswith_replicate()

--- endswith_alternative.py
+++ endswith_alternative.py
@@ -1,3 +1,5 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 #Create a function to replicate endswith()
+def endswith_replicate(prefix):
+    pass
 #Utilize the created function
