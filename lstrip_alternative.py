2ad4ae3b37d44fd38912a769dc3354a0c6055ab8
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 11:23:06 +0800

    Fixed function removing all spaces

--- lstrip_alternative.py
+++ lstrip_alternative.py
@@ -1,9 +1,14 @@
 #Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 #Create a function to replicate lstrip()
 def lstrip_replicate(prefix):
+    strip = True
+    output = ""
     for i in prefix:
-        if i == " ":
-            output = prefix.replace(i, "")
+        if strip and i == " ":
+	    continue
+	else:
+	    strip = False
+            output += i
     print(output)
 #Utilize the created function
 name = "    Marxi   us"
