a37d397c5ffb8d471f560b8f3273b98cfaac3a74
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 16:50:55 +0800

    Created function capitalize_replicate()

--- capitalize_alternative.py
+++ capitalize_alternative.py
@@ -1,3 +1,5 @@
 #Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 #Create a function to replicate capitalize()
+def capitalize_replicate(prefix):
+    pass
 #Utilize the created function
