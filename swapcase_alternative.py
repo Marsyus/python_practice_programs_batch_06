eef84603118f7f1b09c6db5fe69f05a9b2c9aae4
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 16:51:46 +0800

    Created function swapcase_replicate()

--- swapcase_alternative.py
+++ swapcase_alternative.py
@@ -1,3 +1,5 @@
 #Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 #Create a function to replicate swapcase()
+def swapcase_replicate(prefix):
+    pass
 #Utilize the created function
