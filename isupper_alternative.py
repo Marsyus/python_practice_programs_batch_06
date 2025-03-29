18a3c7dc188f62c8dc86c815573621b384180c9c
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 16:54:00 +0800

    Created function isupper_replicate()

--- isupper_alternative.py
+++ isupper_alternative.py
@@ -1,3 +1,5 @@
 #Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 #Create a function to replicate isupper()
+def isupper_replicate(prefix):
+    pass
 #Utilize the created function
