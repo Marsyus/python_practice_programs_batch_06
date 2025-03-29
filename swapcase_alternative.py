86321847258209a3985d7902ff87acf41a94fb18
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 16:55:50 +0800

    Added necessary variables

--- swapcase_alternative.py
+++ swapcase_alternative.py
@@ -1,5 +1,9 @@
 #Prog08. swapcase() reverse the casing of each of the character of the string. Create a program that do the same functionality without using swapcase() function.
 #Create a function to replicate swapcase()
 def swapcase_replicate(prefix):
-    pass
+    output = ""
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    upper_to_lower = str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz")
+    lower_to_upper = str.maketrans("abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 #Utilize the created function
