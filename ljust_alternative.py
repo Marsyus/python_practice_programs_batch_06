f02a1bb479c51ab35d7bc24172a21277def9a2a9
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 17:55:36 +0800

    Replaced 'count' with 'width' and added another parameter 'char'

--- ljust_alternative.py
+++ ljust_alternative.py
@@ -1,5 +1,5 @@
 #Prog06. ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter. Create a program that do the same functionality without using ljust() function.
 #Create a function to replicate ljust()
-def ljust_replicate(prefix, count):
+def ljust_replicate(prefix, width, char):
     pass
 #Utilize the created function
