3f051fe41bd35905a7aac6e8e1bd3dadd1d6bd25
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 11:17:13 +0800

    Created function lstrip_alternate()

--- lstrip_alternative.py
+++ lstrip_alternative.py
@@ -1,3 +1,8 @@
 #Prog01. lstrip() remove the space characters at the beginning of the string. Create a program that do the same functionality without using lstrip() function.
 #Create a function to replicate lstrip()
+def lstrip_replicate(prefix):
+    for i in prefix:
+        if i == " ":
+            output = prefix.replace(i, "")
+    print(output)
 #Utilize the created function
