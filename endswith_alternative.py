a5fec4a07eba1dc884f75111ec29eafdbb6d92b4
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 17:50:26 +0800

    Added 'ending' parameter in the function

--- endswith_alternative.py
+++ endswith_alternative.py
@@ -1,6 +1,6 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 #Create a function to replicate endswith()
-def endswith_replicate(prefix):
+def endswith_replicate(prefix, ending):
     prefix += "x"
     if ending + "x" in prefix:
         print("True")
