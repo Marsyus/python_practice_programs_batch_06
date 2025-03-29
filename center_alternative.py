dbb8194b18e5fc98a7206591c6267138513b4256
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 18:22:24 +0800

    Added necessary variables

--- center_alternative.py
+++ center_alternative.py
@@ -1,5 +1,7 @@
 #Prog07. center() add space characters at the beginning and at the end of the string to print the string at the center. Create a program that do the same functionality without using center() function.
 #Create a function to replicate center()
 def center_replicate(prefix, width, char):
-    pass
+    right = 0
+    left = 0
+    need = width - len(prefix)
 #Utilize the created function
