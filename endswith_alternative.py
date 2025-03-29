50767b01b381348c170fdeb78f1c11fe613c63bd
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 17:48:03 +0800

    Determine the ending of the prefix

--- endswith_alternative.py
+++ endswith_alternative.py
@@ -1,5 +1,9 @@
 #Prog05. endswith() check if the string end part matches the function parameter. Create a program that do the same functionality without using endswith() function.
 #Create a function to replicate endswith()
 def endswith_replicate(prefix):
-    pass
+    prefix += "x"
+    if ending + "x" in prefix:
+        print("True")
+    else:
+        print("False")
 #Utilize the created function
