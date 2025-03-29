b2cd5565a308b19d2cf4fe387c444f8110386013
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 17:17:43 +0800

    Added necessary variables

--- title_alternative.py
+++ title_alternative.py
@@ -1,5 +1,10 @@
 #Prog10. title() makes all first letter of each word in the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using title() function.
 #Create a function to replicate title()
 def title_replicate(prefix):
-    pass
+    output = ""
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    upper_to_lower = str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz")
+    lower_to_upper = str.maketrans("abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    first = True
 #Utilize the created function
