5e5b321ca5efe5673e3c4afe536f64a3f61fb97f
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 17:39:50 +0800

    Added necessary variables

--- isupper_alternative.py
+++ isupper_alternative.py
@@ -1,5 +1,6 @@
 #Prog04. isupper() check if all characters of the string is on upper case. Create a program that do the same functionality without using isupper() function.
 #Create a function to replicate isupper()
 def isupper_replicate(prefix):
-    pass
+    upper_count = 0
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 #Utilize the created function
