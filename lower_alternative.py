892744b9496a55ae5348240ef4e9f106872dbcea
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 11:27:26 +0800

    Created function lower_replicate()

--- lower_alternative.py
+++ lower_alternative.py
@@ -1,3 +1,7 @@
 #Prog03. lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 #Create a function to replicate lower()
+def lower_replicate(prefix):
+    table = str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz")
+    output = prefix.translate(table)
+    print(output)
 #Utilize the created function
