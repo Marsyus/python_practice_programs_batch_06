48391c2ca87f6ae62c678be4079d119f66af82d5
Marsyus <miadolomboy0917@gmail.com>
2025-03-29 17:25:30 +0800

    Added necessary variables

--- capitalize_alternative.py
+++ capitalize_alternative.py
@@ -1,5 +1,10 @@
 #Prog09. capitalize() makes the first letter of the string, capital letter. And all other letter in small case. Create a program that do the same functionality without using capitalize() function.
 #Create a function to replicate capitalize()
 def capitalize_replicate(prefix):
-    pass
+    output = ""
+    uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    lowercase = "abcdefghijklmnopqrstuvwxyz"
+    upper_to_lower = str.maketrans("ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz")
+    lower_to_upper = str.maketrans("abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    first = True
 #Utilize the created function
